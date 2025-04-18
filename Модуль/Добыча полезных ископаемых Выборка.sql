00e39d852d4db3958e1eac02670daf0c94452ee1
MatuxaK <133955743+MatuxaK@users.noreply.github.com>
2025-04-18 10:43:26 +0300

    Delete Модуль directory

--- Модуль/Добыча полезных ископаемых Выборка.sql
+++
@@ -1,6 +0,0 @@
--- 3. Определить, какие ископаемые добываются в Эльдорадо", и добыча является прибыльной
-SELECT M.MineralName
-FROM Minerals M
-JOIN ExtractionPoints EP ON M.MineralID = EP.MineralID
-JOIN Deposits D ON EP.DepositID = D.DepositID
-WHERE D.DepositName = 'Эльдорадо' AND D.CostPerUnit < M.UnitPrice;
--- Модуль/Кулинария Заполнение.sql
+++
@@ -1,9 +0,0 @@
--- Вставка данных (Примеры)
-INSERT INTO Dishes (DishName, Category, Recipe, PortionWeight) VALUES
-('Борщ', 'Первое', 'Рецепт', 300.00);
-
-INSERT INTO Products (ProductName, Category, Calories, UnitPrice, UnitOfMeasure) VALUES
-('Картофель', 'Овощи', 100, 10.00, 'кг');
-
-INSERT INTO DishIngredients (DishID, ProductID, Quantity, AddingOrder, Portions) VALUES
-(1, 1, 0.50, 1, 1);
